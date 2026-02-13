d7fe79a4436be3a6eff661e81e09181ce5ee3d85
Hiram Mendez <hirammendez000@gmail.com>
2026-02-13 07:52:32 -0600

    Add validations

--- main.sh
+++ main.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-git pull
+# git pull
+validateDirname() {
+    local PATH_DIRECTORY=$1
+    if [[ ! -d "$PATH_DIRECTORY" ]]; then
+        echo "$PATH_DIRECTORY doesn't exist"
+        exit 1
+    fi
+}
 
-GIT_FOLDER="/home/hiram/Documents/projects/obsidian-notes-publisher/test-folde"
-
-LOG_FILE="/home/hiram/Documents/projects/obsidian-notes-publisher/activity.log"
-
-DATE="$(date +'%y-%m-%d %r')"
+GIT_FOLDER="/home/hiram/Documents/projects/obsidian-notes-publisher/test-folder"
+validateDirname $GIT_FOLDER
 
 cd $GIT_FOLDER
-# TODO: add validation in case that doesn't exit....
+
+LOG_FILE="/home/hiram/Documents/projects/obsidian-notes-publisher/activity.log"
+LOG_FILE_DIRNAME="$(dirname $LOG_FILE)"
+validateDirname $LOG_FILE_DIRNAME
+
+if [[ ! -f "$LOG_FILE" ]]; then
+    touch $LOG_FILE
+fi
+
+DATE="$(date +'%y-%m-%d %r')"
 
 echo "[$DATE] Running Obsidian-Notes-Publisher Script" >> "$LOG_FILE"
 
@@ -17,22 +30,17 @@ PATTERN='nothing to commit,'
 
 if [[ $STATUS_OUTPUT =~ $PATTERN ]]; then
     echo "[$DATE] Nothing to commit" >> "$LOG_FILE"
-    echo "---------------------------------------------------" >> "$LOG_FILE"
-else
-    git add .
-    # TODO: add validations
-
-    COMMIT_CMD=$(printf 'git commit -m "[BOT] %s"' "$DATE")
-    eval $COMMIT_CMD
-
-    
-    git push
-    # TODO: add validaions
-
-    echo "[$DATE] Commit created and pushed" >> "$LOG_FILE"
-    echo "---------------------------------------------------" >> "$LOG_FILE"
-    
+    echo "--------------------------------------------------------------" >> "$LOG_FILE"
+    exit 0
 fi
 
+git add .
 
-# date +"%y-%m-%d %r"
+COMMIT_CMD=$(printf 'git commit -m "[BOT] %s"' "$DATE")
+eval $COMMIT_CMD
+
+git push
+
+echo "[$DATE] Commit created and pushed" >> "$LOG_FILE"
+echo "--------------------------------------------------------------" >> "$LOG_FILE"
+exit 0
